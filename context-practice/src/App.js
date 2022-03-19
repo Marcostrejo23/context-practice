d57990a0dbd58e79771bddb527b93da18c65f5f9
Marcos Trejo <marcostrejo23@gmail.com>
2022-03-18 18:57:22 -0700

    test for provider

--- context-practice/src/App.js
+++ context-practice/src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./App.css";
-
+import FunctionContextComponent from '';
 function App() {
+
+  function toggleTheme() {
+    setDarkTheme((prevDarkTheme) => !prevDarkTheme);
+  }
   return <div className="App"></div>;
 }
 
